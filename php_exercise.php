c9ba33924d9924084276fba21a6e07512c9f20cf
Jarry Cabaluna <janjalani.yonbanme01@gmail.com>
2022-09-20 16:54:14 +0800

    final

---
+++ php_exercise.php
@@ -0,0 +1,49 @@
+<?php
+echo "PHP exercise 1\n";
+echo "Twinkle, Twinkle little star. \n";
+$twinkle = "Twinkle";
+$star = "star";
+echo "$twinkle $star\n";
+$twinkle = "no";
+$star = "yes";
+echo "$twinkle $star\n\n";
+
+echo "PHP exercise 2\n";
+$x=10;
+$y=7;
+$sum= $x + $y;
+$dif= $x - $y;
+$prd= $x * $y;
+$quo= $x / $y;
+$mod= $x % $y;
+echo "$x + $y = ".$sum."\n";
+echo "$x - $y = ".$dif."\n";
+echo "$x * $y = ".$prd."\n";
+echo "$x / $y = ".$quo."\n";
+echo "$x % $y = ".$mod."\n\n";
+
+
+echo "PHP exercise 3\n";
+$variable = 8;
+echo "Value is now $variable.\n";
+$variable = $variable + 2;
+echo "Value is now $variable.\n";
+$variable = $variable - 4;
+echo "Value is now $variable.\n";
+$variable = $variable * 5;
+echo "Value is now $variable.\n";
+$variable = $variable / 3;
+echo "Value is now $variable.\n";
+$variable = $variable + 1;
+echo "Value is now $variable.\n";
+$variable = $variable - 1;
+echo "Value is now $variable.\n\n";
+
+
+echo "PHP exercise 4\n";
+echo "string(5) ".'"Harry"'."\n";
+echo "Harry\n";
+echo "int(28)\n";
+echo "NULL";
+
+?>
